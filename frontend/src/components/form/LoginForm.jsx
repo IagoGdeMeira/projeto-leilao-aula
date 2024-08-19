8751134acfd31decd831f68ec772f942d9086c4b
IagoGdeMeira <iagomeiraifpr@gmail.com>
2024-08-19 18:41:58 -0300

    Removendo props das instâncias do componentes em LoginForm.jsx. Devem ser adicionadas novamente seguindo os padrões de PropType já estabelecidos

--- frontend/src/components/form/LoginForm.jsx
+++ frontend/src/components/form/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./LoginForm.css";
 import Form from "./Form";
 import FormButton from "./form_button/FormButton";
 import FormField from "./form_field/FormField";
@@ -11,32 +12,10 @@ function LoginForm() {
     return (
         <Form className="LoginForm" name={formName}>
             <h2>Acesse sua conta</h2>
-            <FormField
-                label="E-mail"
-                name="email"
-                placeholder="Digite seu endereço de e-mail aqui."
-                required={true}
-            />
-            <FormField
-                label="Senha"
-                name="password"
-                placeholder="Digite sua senha aqui."
-                required={true}
-            />
-            <FormButton
-                ariaLabel="Cancelar login e retornar à página inicial."
-                disabled={false}
-                form={formName}
-                text="Cancelar"
-                type="button"
-            />
-            <FormButton
-                ariaLabel="Realizar login com as informações preenchidas."
-                disabled={submissionDisabled}
-                form={formName}
-                text="Entrar"
-                type="submit"
-            />
+            <FormField/>
+            <FormField/>
+            <FormButton/>
+            <FormButton/>
         </Form>
     );
 }
